cfd6e8afcce26cd4d2c3fae21ffccbac94c597cb
dimitristrechas <dtrechas@singularlogic.eu>
2020-11-03 17:16:15 +0200

    npm update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 
 const IndexPage: FC = () => {
-  return <h1 className="text-red-500">Hello World!</h1>;
+  return <h1 className="text-red-500">Hello Next.js - Typescript - TailwindCSS starter!</h1>;
 };
 
 export default IndexPage;
