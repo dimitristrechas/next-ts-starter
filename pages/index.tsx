d8717259accbbdd9a02f7035ad4f25b816caf0ac
Dimitris Trechas <dimitristrechas@gmail.com>
2020-11-19 14:32:11 +0200

    rm -rf

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import { FC } from "react";
 
 const IndexPage: FC = () => {
-  return <h1 className="text-red-900 m-5">Hello Next.js - Typescript - TailwindCSS starter!</h1>;
+  return (
+    <div className="m-5 p-5">
+      <span className="text-red-500 p-1 border-2 border-blue-500">
+        Hello Next.js - Typescript - TailwindCSS starter!
+      </span>
+    </div>
+  );
 };
 
 export default IndexPage;
